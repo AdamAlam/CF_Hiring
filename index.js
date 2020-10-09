c2e1275307e08e6187f3ba51b9d3e1b7aa0c8d98
AdamAlam <46464084+AdamAlam@users.noreply.github.com>
2020-10-09 16:38:44 -0500

    Added try catch, chained HTMLRewriter, Passed In links arr to LinksTransformer(), Changed Routing Logic

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let links = [
+const links = [
   {
     name: 'Google',
     url: 'https://www.google.com',
@@ -20,7 +20,7 @@ class LinksTransformer {
 
   async element(element) {
     element.setInnerContent(
-      links.map(link => `<a href="${link.url}">${link.name}</a>`).join(''),
+      this.links.map(link => `<a href="${link.url}">${link.name}</a>`).join(''),
       { html: true },
     )
   }
@@ -56,29 +56,25 @@ addEventListener('fetch', event => {
  */
 
 async function handleRequest(request) {
-  // return new Response()
-  let url = request.url
-  if (
-    url.substr(url.length - 5) == 'links' ||
-    url.substr(url.length - 6) == 'links/'
-  ) {
+  let url = new URL(request.url)
+  let path = url.pathname
+  if (path === '/links' || path === '/links/') {
     return new Response(JSON.stringify(links), {
       headers: { 'content-type': 'application/json;charset=UTF-8' },
     })
   } else {
-    const res = await fetch('https://static-links-page.signalnerve.workers.dev')
-    let this_html = new HTMLRewriter()
-      .on('div[id="links"]', new LinksTransformer())
-      .transform(res)
-    this_html = new HTMLRewriter()
-      .on("div[id='profile']", new RemoveDisplayNone())
-      .transform(this_html)
-    this_html = new HTMLRewriter()
-      .on("img[id='avatar']", new ImageTransformer())
-      .transform(this_html)
-    this_html = new HTMLRewriter()
-      .on("h1[id='name']", new NameTransformer())
-      .transform(this_html)
-    return this_html
+    try {
+      const res = await fetch(
+        'https://static-links-page.signalnerve.workers.dev',
+      )
+      return new HTMLRewriter()
+        .on('div[id="links"]', new LinksTransformer(links))
+        .on("div[id='profile']", new RemoveDisplayNone())
+        .on("img[id='avatar']", new ImageTransformer())
+        .on("h1[id='name']", new NameTransformer())
+        .transform(res)
+    } catch (err) {
+      throw err
+    }
   }
 }
